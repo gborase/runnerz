dcd189f0b21e63da7f79620b3a24950ad415beb2
Ganesh <gborase81@gmail.com>
2026-09-02 21:45:08 +0530

    Added the files

---
+++ src/main/java/com/ganesh/tech/runnerz/RunnerzApplication.java
@@ -0,0 +1,14 @@
+package com.ganesh.tech.runnerz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnerzApplication {
+
+	public static void main(String[] args) {
+		 SpringApplication.run(RunnerzApplication.class, args);
+		 
+	}
+
+}
---
+++ src/test/java/com/ganesh/tech/runnerz/RunnerzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganesh.tech.runnerz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnerzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
